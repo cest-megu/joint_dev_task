3a162f0c43ac76e72ba9d5dc2e490db55de91d67
Megurin <tutimur_0521@yahoo.co.jp>
2021-03-09 10:55:29 +0900

    q17とq18を回答

--- task.rb
+++ task.rb
@@ -164,6 +164,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
+  attr_accessor :name, :age, :gender
+
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+  def info
+    puts "名前: #{@name}"
+    puts "年齢: #{@age}"
+    puts "性別: #{@gender}"
+  end
 
 end
 
@@ -179,6 +192,20 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  attr_accessor :name, :age
+
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    if @age > 30
+      puts "こんにちは, #{@name}と申します。宜しくお願いいたします。"
+    else
+      puts "はいさいまいど〜, #{@name}です！！"
+    end
+  end
 
 end
 
